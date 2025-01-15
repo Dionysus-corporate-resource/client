cdbe5ed15835a17f15708d599629502044bb3708
Roman <skorohodroman921@gmail.com>
2025-01-15 21:50:43 +0300

    installed shadcn and tailwind

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,11 @@
 import { NavLink } from "react-router";
+import { Button } from "../shared/components/ui/button";
 
 export default function HomePage() {
   return (
     <div className="space-x-2">
       <span>HomePage</span>
+      <Button>Click me</Button>
       <NavLink to="/login">login</NavLink>
       <NavLink to="/register">register</NavLink>
     </div>
